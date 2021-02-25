f708931701413cabaff2d68a1d453618014b77a9
Daniel Helc - SkyPapi <daniel.helc@skypapi.com>
2021-02-25 14:10:10 -0500

    Initial Commit

---
+++ FireStoreDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireStoreDemo
+//
+//  Created by Daniel Helc on 2021-02-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
